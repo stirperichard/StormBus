d564465ef87ead2fa5fe445b37d8100e639a9dd4
afjcjsbx@gmail.com <Chiaretta15?>
2020-07-06 22:19:56 +0200

    1.0.1.5

--- scripts/createTopic.sh
+++ scripts/createTopic.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic query-1-input
-docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic query-1-output
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic query-2-input
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic query-3-input
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic query-1-output-daily
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic query-1-output-weekly
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic query-1-output-monthly
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic query-2-output-daily
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic query-2-output-weekly
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic query-3-output-daily
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic query-3-output-weekly
--- scripts/deleteTopic.sh
+++ scripts/deleteTopic.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-/Users/simone/kafka_2.11-2.2.0/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic query-1-input
-/Users/simone/kafka_2.11-2.2.0/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic query-1-output
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic query-1-input
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic query-2-input
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic query-3-input
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic query-1-input-daily
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic query-1-input-weekly
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic query-1-input-monthly
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic query-2-input-daily
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic query-2-input-weekly
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic query-3-input-daily
+docker exec --user=root kafka0 kafka/bin/kafka-topics.sh --delete --zookeeper localhost:2181 --topic query-3-input-weekly
